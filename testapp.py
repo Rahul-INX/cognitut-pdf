17fd8b3e79c98615cedaf0e316d371cf2b322f89
Rahul-INX <rahul.harivansh2001@gmail.com>
2024-01-01 02:26:44 +0530

    BUGFIX: context not passed to llm(hotfix), creation of prompt for model behaviour and response generatiom,doc_mode mode functioning, chat history implemented . TODO: connect with gsheets and create code to upload data

--- testapp.py
+++ testapp.py
@@ -1,24 +1,19 @@
-from langchain.document_loaders import PyPDFDirectoryLoader, TextLoader, CSVLoader
+from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
-from langchain.storage import LocalFileStore
-from langchain.retrievers import BM25Retriever, EnsembleRetriever, ContextualCompressionRetriever
-from langchain.retrievers.document_compressors import LLMChainExtractor, LLMChainFilter, EmbeddingsFilter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.retrievers import (BM25Retriever,EnsembleRetriever,ContextualCompressionRetriever)
+from langchain.retrievers.document_compressors import (LLMChainExtractor,EmbeddingsFilter)
 from langchain.llms import DeepInfra
-from langchain.chains import ConversationalRetrievalChain
-from langchain_core.utils.env import get_from_env
 from dotenv import load_dotenv
 import streamlit as st
-from streamlit_chat import message
 import os
-import pandas as pd
 
 load_dotenv()
 
 # Accessing the OPENAI_API_KEY and DEEPINFRA_API_TOKEN variables
-openaiapikey = os.environ.get('OPENAI_API_KEY')
-deeptoken = os.environ.get('DEEPINFRA_API_TOKEN')
+openaiapikey = os.environ.get("OPENAI_API_KEY")
+deeptoken = os.environ.get("DEEPINFRA_API_TOKEN")
 
 # Accessing the DEEPINFRA_API_TOKEN variable
 compressor_llm = DeepInfra(model_id="mistralai/Mistral-7B-Instruct-v0.1")
@@ -29,7 +24,7 @@ compressor_llm.model_kwargs = {
     "top_p": 0.90,
 }
 
-embeddings = OpenAIEmbeddings(model='text-embedding-ada-002')
+embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 # Check if environment variables are set
 if openaiapikey is None or deeptoken is None:
@@ -58,8 +53,8 @@ st.set_page_config(layout="wide", page_title="COGNITUT")
 st.title(":red[COGNITUT]")
 
 # Display initial chat message
-with st.chat_message("ai", avatar='👨‍🏫'):
-    st.write('Hi, how may I help you today ?')
+with st.chat_message("ai", avatar="👨‍🏫"):
+    st.write("Hi, how may I help you today ?")
 
 # Initialize Streamlit session state
 if "messages" not in st.session_state:
@@ -79,49 +74,42 @@ if doc_mode:
 
     # NOTE: THE KEY OF THE DICTIONARY IS CASE-SENSITIVE
     all_subjects = {
-        'CSE': {
-            'Semester 7': ['Cloud Computing', "Principles Of Management", 'Cyber Forensics', 'Cryptography And Network Security', 'Blockchain Technology'],
-            # Add subjects for other semesters as needed
+        "CSE": {"Semester 7": ["Cloud Computing","Principles Of Management","Cyber Forensics","Cryptography And Network Security","Blockchain Technology",],
         },
-        'IT': {
+        "IT": {
             # 'Semester 5': ['IT Subject1', 'IT Subject2', 'IT Subject3'],
             # 'Semester 6': ['IT SubjectA', 'IT SubjectB', 'IT SubjectC'],
             # Add subjects for other semesters as needed
         },
-        # Add subjects for other departments as needed
     }
 
     # Department selection
     department = st.sidebar.selectbox(
-        'Choose your department (only CSE support yet)',
-        ('CSE', 'IT', 'ECE', 'MECH', 'CSBS', 'AI & DS', 'EEE', 'BME'),
-        index=0
+        "Choose your department (only CSE support yet)",
+        ("CSE", "IT", "ECE", "MECH", "CSBS", "AI & DS", "EEE", "BME"),
+        index=0,
     )
 
     # Semester selection
     semester = st.sidebar.selectbox(
-        'Choose your current semester (only 7th sem support yet)',
-        ('Semester 1', 'Semester 2', 'Semester 3', 'Semester 4', 'Semester 5', 'Semester 6', 'Semester 7', 'Semester 8'),
-        index=6
-    )
+        "Choose your current semester (only 7th sem support yet)",
+        ("Semester 1","Semester 2","Semester 3","Semester 4","Semester 5","Semester 6","Semester 7","Semester 8",),index=6)
 
     # Subject selection based on the selected department and semester
     subject_options = all_subjects.get(department, {}).get(semester, [])
-    subject = st.sidebar.selectbox('Choose your subject', subject_options)
+    subject = st.sidebar.selectbox("Choose your subject", subject_options)
 
     # Chat model selection
     chat_model_name = st.sidebar.selectbox(
-        'Choose Chat Model',
-        ('meta-llama/Llama-2-7b-chat-hf', 'mistralai/Mistral-7B-Instruct-v0.1'),
-        index=0
-    )
+        "Choose Chat Model",
+        ("meta-llama/Llama-2-7b-chat-hf","mistralai/Mistral-7B-Instruct-v0.1","meta-llama/Llama-2-13b-chat-hf",),index=0)
 
     if department and semester and subject:
         # Path to pdf documents
         doc_path = f"Documents/{department}/{semester}/{subject}"
 
         # Define the path for the FAISS vector store
-        FAISS_DB_PATH = f'vectorStore/{department}/{semester}/{subject}'
+        FAISS_DB_PATH = f"vectorStore/{department}/{semester}/{subject}"
 
         # Check if the directory exists, and create it if it doesn't
         if not os.path.exists(doc_path):
@@ -143,33 +131,28 @@ if doc_mode:
                     db.save_local(FAISS_DB_PATH)
 
                     st.balloons()
-                    st.toast(f"Vector DB created in: {FAISS_DB_PATH[12:]}", icon='✅')
+                    st.toast(f"Vector DB created in: {FAISS_DB_PATH[12:]}", icon="✅")
 
             else:
                 st.warning("Document directory is empty. No documents found.")
+                st.toast(f"WARNING : Empty Directory\nLoad Files In {doc_path}", icon="⚠")
         else:
             # FAISS database is not empty
             db = FAISS.load_local(FAISS_DB_PATH, embeddings)
-            st.toast(f"Vector DB loaded from: {FAISS_DB_PATH[12:]}", icon='✅')
-
             faiss_retriever = db.as_retriever(search_kwargs={"k": 5})
 
-            with st.spinner("Documents are loading........"):
-                # Load documents using the cached function 
-                documents = doc_loader(doc_path)
+            with st.spinner(f"**Processing ':orange[{doc_path.split('/')[-1]}]'........**"):
 
-                st.toast(f"Documents loaded.....", icon='✅')
+                # Load documents using the cached function
+                documents = doc_loader(doc_path)
 
                 bm25_retriever = BM25Retriever.from_documents(documents)
                 bm25_retriever.k = 3
 
                 # initialize the ensemble retriever
-                ensemble_retriever = EnsembleRetriever(
-                    retrievers=[bm25_retriever, faiss_retriever], 
-                    weights=[0.2, 0.8]
-                    )
+                ensemble_retriever = EnsembleRetriever(retrievers=[bm25_retriever, faiss_retriever], weights=[0.2, 0.8])
 
-                user_query = st.chat_input('Enter your query here ....')
+                user_query = st.chat_input("Enter your query here ....")
 
                 if user_query is not None:
                     # Continue with the code execution
@@ -181,35 +164,139 @@ if doc_mode:
                     else:
                         # Compression mode is disabled
                         compressor = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=0.75)
-                        print("using similarity_threshold")
 
                     compression_retriever = ContextualCompressionRetriever(base_compressor=compressor, base_retriever=ensemble_retriever)
                     compressed_docs = compression_retriever.get_relevant_documents(user_query)
 
                     metadata_info = []
-                    for i in range(len(compressed_docs)):    
-                        metadata_info.append('\''+str(compressed_docs[i].metadata)[23:])
+                    for i in range(len(compressed_docs)):
+                        metadata_info.append("'" + str(compressed_docs[i].metadata)[23:])
 
                     def chat_model(model_name):
                         llm = DeepInfra(model_id=model_name)
                         llm.model_kwargs = {
-                            "temperature": 0.2,
+                            "temperature": 0.1,
                             "repetition_penalty": 1.2,
-                            "max_new_tokens": 512,
+                            "max_new_tokens": 1024,
                             "top_p": 0.9,
                         }
                         return llm
 
                     llm = chat_model(chat_model_name)
 
-                    context = "\n".join([f"{doc.page_content}\nMetadata: {doc.metadata}" for doc in compressed_docs])
-                    response = llm(context=context, prompt=user_query)
+                    context = "\n".join([f"{doc.page_content}\nMetadata: {doc.metadata}\n"for doc in compressed_docs])
+                    system_message_inst = f"""### Role: Specialized Academic Expert Bot
+**Description:**
+Specialized Academic Expert Bot dedicated to in-depth knowledge in the {subject} domain, delivering precise and structured information. Committed to addressing user queries with clarity and aligning responses with academic principles.
+### Task:
+1. **Subject-specific Contextual Mastery:**
+   - Master the art of extracting relevant information in the {subject} context.
+   - Base responses meticulously on the provided {subject} context.
+   - Use the relevant information to give a comprehensive answer, adhering strictly to academic standards.
+2. **Thorough {subject} Responsiveness:**
+   - Demonstrate a commitment to addressing user queries comprehensively in the {subject} field.
+   - Utilize {subject}-specific context extensively to provide detailed responses.
+   - Stay within the defined context range and avoid making assumptions beyond the provided information.
+3. **Art of {subject} Language:**
+   - Employ clear and concise language tailored for the {subject} academic audience.
+   - Prioritize clarity through strategic use of headings, markdown, subheadings, paragraphs, and bullet points.
+   - Utilize markdown techniques such as '#' for titles, '*' for highlighting, and '**' for bolding.
+4. **Handling Irrelevance in {subject} Context:**
+   - Address situations where the {subject} context lacks relevance by responding with "NOT ENOUGH INFORMATION COULD BE FOUND IN THE {subject} CONTEXT..."
+   - Avoid providing information beyond the specified context range.
+5. **Architectural Clarity in {subject} Responses:**
+   - Craft responses with a robust structure, specific to the {subject} field.
+   - Ensure that responses are accessible and understandable to individuals without prior {subject} knowledge.
+6. **Guiding Academic Principles in {subject} Expertise:**
+   - Uphold academic principles in every response, maintaining a high standard of accuracy and reliability in the {subject} domain.
+   - Do not hallucinate on information; stay within the confines of the provided context.
+### Necessity:
+   - In the presence of explicit language, comments, vulgar slang, or harmful information, employ the response: 'I Am a responsible AI. Hence, cannot help you with that' and conclude the response.
+"""
+                    response = llm(
+                        f"""'<->' symbolizes a logical seperation between 2 types of data |
+                                   Instrustions to keep in mind for generation:'{system_message_inst}'<->
+                                   Context = '{context}'<->
+                                   Query = {user_query}
+                                   """)
 
-                    # Save user input and LM output to session state
-                    st.session_state.messages.append({"role": "user", "content": user_query})
-                    st.session_state.messages.append({"role": "ai", "content": response})
-                    # Display LM output
-                    with st.chat_message("ai", avatar='👨‍🏫'):
-                        st.markdown(response)
-                    with st.expander("Click To Show Context"):
-                        st.write(context)
+                    if user_query is not None:
+                        # Save user input and LM output to session state
+                        st.session_state.messages.append({"role": "user", "content": user_query})
+                        st.session_state.messages.append({"role": "ai", "content": response})
+
+                        # Display LM output
+                        with st.chat_message("user", avatar="🟢"):
+                            st.markdown(user_query)
+                        with st.chat_message("ai", avatar="👨‍🏫"):
+                            st.markdown(response)
+                            st.write(metadata_info)
+                        with st.expander("SHOW CONTEXT"):
+                            st.write(context)
+
+else:
+    chat_model_name = st.sidebar.selectbox(
+        "Choose Chat Model",("meta-llama/Llama-2-7b-chat-hf","mistralai/Mistral-7B-Instruct-v0.1","meta-llama/Llama-2-13b-chat-hf", ),index=0)
+    user_query = st.chat_input("Enter your query here ....")
+
+    def chat_model(model_name):
+        llm = DeepInfra(model_id=model_name)
+        llm.model_kwargs = {
+            "temperature": 0.7,
+            "repetition_penalty": 1.2,
+            "max_new_tokens": 1024,
+            "top_p": 0.85,
+        }
+        return llm
+
+    llm = chat_model(chat_model_name)
+    if user_query is not None:
+        with st.spinner(f"**'DOCUMENT MODE =  :red[OFF] | :green[Generating Response]......'**"):
+            system_message_inst = """# Role: Academic Expert Bot
+**Description:**
+Dedicated Comprehensive Academic Expert Bot with a profound understanding of various subjects, committed to delivering high-quality, detailed responses. This bot excels in providing precise and structured information, aligning answers with the highest academic standards.
+## Task:
+1. **Subject-specific Contextual Mastery:**
+   - Master the art of extracting relevant information in any academic context.
+   - Base responses meticulously on the provided subject context.
+   - Utilize relevant information to deliver comprehensive answers adhering to academic standards.
+2. **Thorough Responsiveness:**
+   - Demonstrate a commitment to addressing user queries comprehensively across diverse academic fields.
+   - Utilize subject-specific context extensively to provide detailed, well-informed responses.
+3. **Art of Academic Language:**
+   - Employ clear and concise language tailored for a diverse academic audience.
+   - Prioritize clarity through strategic use of headings, markdown, subheadings, paragraphs, and bullet points.
+   - Use markdown techniques for titles (#), highlighting (*), and bolding (**).
+4. **Handling Irrelevance in Academic Context:**
+   - Address situations where the academic context lacks relevance by responding with "NOT ENOUGH INFORMATION COULD BE FOUND IN THE CONTEXT..."
+5. **Architectural Clarity in Responses:**
+   - Craft responses with a robust structure applicable to various academic fields.
+   - Ensure that responses are accessible and understandable to individuals without prior knowledge in a specific subject.
+6. **Guiding Academic Principles:**
+   - Uphold academic principles in every response, maintaining a high standard of accuracy, reliability, and depth across disciplines.
+## Markdown Techniques Explanation:
+   - Use '#' for titles, e.g., #Title#
+   - For highlighting and bolding, use '*', e.g., *Highlighted* or **Bolded**.
+## Necessity:
+   - In the presence of explicit language, comments, vulgar slang, or harmful information, respond with 'I Am a responsible AI. Hence, cannot help you with that' and conclude the response.
+"""
+            response = llm(
+                        f"""'<->' symbolizes a logical seperation between 2 types of data |
+                                   Instrustions to keep in mind for generation:'{system_message_inst}'<->
+                                   Query = {user_query}
+                                   """)
+
+
+
+
+
+            if user_query is not None:
+                # Save user input and LM output to session state
+                st.session_state.messages.append({"role": "user", "content": user_query})
+                st.session_state.messages.append({"role": "ai", "content": response})
+                # Display LM output
+                with st.chat_message("user", avatar="🟢"):
+                    st.markdown(user_query)
+                with st.chat_message("ai", avatar="👨‍🏫"):
+                    st.markdown(response)
+                st.rerun()
